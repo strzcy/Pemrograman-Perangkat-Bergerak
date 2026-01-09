ac1f5a996c03f92003491a61c633a72f5a5bce65
strzcy <desipuspita46832@gmail.com>
2026-01-09 18:05:56 +0700

    Update 2025_10_15_035250_create_products_table.php

--- database/migrations/2025_10_15_035250_create_products_table.php
+++ database/migrations/2025_10_15_035250_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")->constrained()->onDelete("cascade");
+            $table->foreignId("id")->constrained()->onDelete("cascade");
             $table->string('name');
             $table->string('slug');
             $table->decimal('price');
